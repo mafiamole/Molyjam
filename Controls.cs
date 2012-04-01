cdc2dd8be735a43104372a119c81956f906cee1d
Paul <mafiamole@gmail.com>
2012-04-01 14:58:22 +0100

    Adding controls and lives classes

---
+++ Controls.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace RenderTarget2DSample
+{
+    class Controls
+    {
+        public Controls() { }
+
+        public void Update(ref Vector2 direction,ref KeyboardState kbstate,ref GlassesUI glasses,ref Vector2 speed, int PlayrBaseSpeed,ref bool characterJumped)
+        {
+            // Grab current keyboar state on loop
+            kbstate = Keyboard.GetState();
+
+            // To prevent the character moving on for ever
+            direction.X = 0;
+            direction.Y = 0;
+            if (kbstate.IsKeyDown(Keys.Left))
+            {
+                direction.X = 1;
+            }
+            if (kbstate.IsKeyDown(Keys.Right))
+            {
+
+                direction.X = -1;
+            }
+
+            if (kbstate.IsKeyDown(Keys.D0)) { Console.Clear(); }
+
+
+
+            if (kbstate.IsKeyDown(Keys.D1))
+            {
+                glasses.SelectGlasses = 0;
+            }
+            else if (kbstate.IsKeyDown(Keys.D2))
+            {
+                glasses.SelectGlasses = 1;
+            }
+            else if (kbstate.IsKeyDown(Keys.D3))
+            {
+                glasses.SelectGlasses = 2;
+            }
+            else if (kbstate.IsKeyDown(Keys.D4))
+            {
+                glasses.SelectGlasses = 3;
+            }
+
+            if (kbstate.IsKeyDown(Keys.LeftShift))
+            {
+                speed.X = PlayrBaseSpeed * 10;
+            }
+            if (kbstate.IsKeyUp(Keys.LeftShift))
+            {
+                speed.X = PlayrBaseSpeed;
+            }
+
+            if (kbstate.IsKeyDown(Keys.Up))
+            {
+                characterJumped = true;
+            }
+            else
+            {
+                characterJumped = false;
+            }
+
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+}
---
+++ Lives.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace RenderTarget2DSample
+{
+    class Lives
+    {
+        private int lifeCount = 2;
+        private Vector2 respawnPosition = new Vector2(100,100); // Should set to map start point initially
+        private ArrayList lifeSprites = new ArrayList();
+        private GameOver gameOver;
+
+        public Lives(Game game, SpriteBatch spriteBatch, SpriteFont spriteFont)
+        {
+            gameOver = new GameOver(game,spriteBatch,spriteFont);
+            game.Components.Add(gameOver);
+
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            // call life sprites.draw
+            gameOver.Draw(gameTime);
+
+        }
+
+
+
+
+        //Life sprite class?
+
+
+        public int NumberOfLives
+        {
+            get
+            {
+                return lifeCount;
+            }
+            set
+            {
+                lifeCount = value;
+            }
+        }
+
+
+
+        private class GameOver  : DrawableGameComponent
+        {
+            SpriteBatch spriteBatch;
+            SpriteFont spriteFont;
+            bool isGameOver = true;
+           
+            public bool IsGameOver{ get{ return isGameOver; } set { isGameOver = value; } }
+
+            public GameOver(Game game, SpriteBatch Batch, SpriteFont Font) : base(game){
+                    spriteFont = Font;
+			        spriteBatch = Batch;
+            }
+
+
+            public override void Draw(GameTime gameTime)
+            {
+                if (IsGameOver)
+                {
+                    string str = "GAME OVER";
+                    spriteBatch.DrawString(spriteFont, str, new Vector2(100, 210), Color.Red, 0.0f, new Vector2(0, 0), 5.0f, new SpriteEffects(), 0.0f);
+                    spriteBatch.DrawString(spriteFont, str, new Vector2(90,200),Color.Yellow,0.0f,new Vector2(0,0),5.0f,new SpriteEffects(),0.0f);
+                }
+            }
+
+        }
+
+    }
+}
